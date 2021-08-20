5432b8b380905a7e1f098d1ecccd931c1f8b6a2d
nimit2801 <mnimitsavant@outlook.com>
2021-08-20 16:26:13 +0530

    Bot complete ✅

---
+++ src/bot.js
@@ -0,0 +1,45 @@
+import {Client, Intents } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+client.on('ready', async()=> {
+    console.log('Ready');
+});
+
+const PREFIX = '$';
+
+let mems = ['Nimit#4979', 'krish#1234'];
+
+client.on('messageCreate', (message) => {
+    let command = message.content.split(' ');
+    console.log(command);
+    if(message.content.startsWith(PREFIX)) {
+        switch(message.content.split(' ')[1]){
+        
+            case 'hello': message.channel.send('Lets start with the first clue! \nGo to this link: https://linktr.ee/welcome2gdscnuv');
+                break;
+            case 'GDSCNUVGG': {
+                message.channel.send("GG! That would've been fun to explore let's move on to the next clue. ");
+                message.channel.send({
+                    files: ['https://cdn.discordapp.com/attachments/873390286708043786/878228570554654720/Screenshot_1629394415.png']
+                })
+            }
+            case 'https://gdsc.community.dev/navrachana-university-vadodara/': {
+                message.channel.send(`Congrats ${message.author} on completing the treasure hunt!`);
+                mems.push(message.author);
+            }
+                break;
+            case 'mems': {
+                message.channel.send(`${mems}`);
+            }
+            break;
+            default: {
+                message.channel.send('Wrong command!')
+            }
+                break;
+        }
+    }
+})
+
+client.login(process.env.TOKEN);
